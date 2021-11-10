9fc6f3da39c421ccdd68dd7873b50a838106fb56
Taylor Noj <taylornoj@gmail.com>
2021-11-10 04:39:28 +0000

    add endpoint to handle POST to login

--- express_server.js
+++ express_server.js
@@ -1,16 +1,19 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const PORT = 8080; // default port 8080
-app.set("view engine", "ejs");
 
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -52,6 +55,12 @@ app.post("/urls/:id", (req, res) => {
 app.post("/urls/:shortURL/delete", (req, res) => {
   const shortURL = req.params.shortURL;
   delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
+//
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
   res.redirect('/urls');
 });
 
